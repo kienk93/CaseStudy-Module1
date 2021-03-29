afd0afc156ba11caffd26bd613ebc9bf68fdf27a
kienk93 <kienk93@users.noreply.github.com>
2021-03-29 10:03:41 +0700

    add animation amelia + change image gura + change song

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ canvas.addEventListener('mousedown', function(event){
     mouse.x = event.x - canvasPosition.left;
     mouse.y = event.y - canvasPosition.top;
 })
-console.log(mouse.x,mouse.y)
+// console.log(mouse.x,mouse.y)
 canvas.addEventListener('mouseup',function(){
     mouse.click = false;
 })
@@ -35,7 +35,6 @@ class Player {
         this.x = canvas.width/2;
         this.y = canvas.height/2;
         this.radius = 50;
-        this.angle = 0;
         this.frameX = 0;
         this.frameY = 0;
         this.spriteWidth = 500;
@@ -65,15 +64,15 @@ class Player {
         ctx.fill();
         ctx.closePath();
         ctx.fillRect(this.x, this.y, this.radius,10);
+        Math.floor(Math.random() * 4)
         ctx.drawImage(amelia
-        , this.frameX * this.spriteWidth
+        , this.frameX + Math.floor(Math.random() * 2)* this.spriteWidth
         , this.frameY * this.spriteHeight
         , this.spriteWidth, this.spriteHeight
         , this.x -70, this.y - 50
         , this.spriteWidth/4, this.spriteHeight/5);
-        
 
-    
+
     }
 }
 const player = new Player();
@@ -85,6 +84,18 @@ const gura2 = new Image();
 gura2.src = 'gura2.jpg'
 const gura3 = new Image();
 gura3.src = 'gura3.jpg'
+const gura4 = new Image();
+gura4.src = 'gura4.jpg';
+const gura5 = new Image();
+gura5.src = 'gura5.jpg'
+const gura6 = new Image();
+gura6.src = 'gura6.jpg'
+const gura7 = new Image();
+gura7.src = 'gura7.jpg';
+const gura8 = new Image();
+gura8.src = 'gura8.jpg'
+const gura9 = new Image();
+gura9.src = 'gura9.jpg'
 const bubblesArray = [];
 class Bubble {
     constructor() {
@@ -94,7 +105,6 @@ class Bubble {
         this.speed = Math.random() * 5 + 1;
         this.distance;
         this.counted = false;
-        this.sound = 'sound1'
     
     }
     update() {
@@ -113,25 +123,53 @@ class Bubble {
         ctx.stroke();
         ctx.save();
         if(this.x > mouse.x) {
-            ctx.drawImage(gura,this.x-30,this.y-30,80,80)
+                var a = ctx.drawImage(gura,this.x-30,this.y-30,80,80)
         } else {
-            ctx.drawImage(gura2,this.x-30,this.y-30,80,80)
-        }
+                var b = ctx.drawImage(gura2,this.x-30,this.y-30,80,80)
+                }
         if(this.y < mouse.y) {
-            ctx.drawImage(gura3,this.x-30,this.y-30,80,80)
+                var c = ctx.drawImage(gura3,this.x-30,this.y-30,80,80)
+        }
+        if(score > 49 && score < 100) {
+            if (this.x > mouse.x) {
+                a = ctx.drawImage(gura4, this.x - 30, this.y - 30, 80, 80)
+            } else {
+                b = ctx.drawImage(gura5, this.x - 30, this.y - 30, 80, 80)
+            }
+            if (this.y < mouse.y) {
+                c = ctx.drawImage(gura6, this.x - 30, this.y - 30, 80, 80)
+
+            }
+        }
+        if(score > 100) {
+            if (this.x > mouse.x) {
+                a = ctx.drawImage(gura7, this.x - 30, this.y - 30, 80, 80)
+            } else {
+                b = ctx.drawImage(gura8, this.x - 30, this.y - 30, 80, 80)
+            }
+            if (this.y < mouse.y) {
+                c = ctx.drawImage(gura9, this.x - 30, this.y - 30, 80, 80)
+
+            }
 
         }
+
+
+
+
     }
 }
 const bubbleSound = document.createElement('audio');
 bubbleSound.src = 'Amelia sings Gura Doesnt Know.mp3'
 const bubbleSound2 = document.createElement('audio');
-bubbleSound2.src = 'pop.mp3'
+bubbleSound2.src = 'I Do Adore.mp3'
+const bubbleSound3 = document.createElement('audio');
+bubbleSound3.src = 'The Lazy Song.mp3'
 
 function handleBubbles () {
     if (gameFrame % 50 == 0){
         bubblesArray.push(new Bubble());
-        console.log(bubblesArray.length)
+        // console.log(bubblesArray.length)
     }
     for (let i=0; i<bubblesArray.length;i++) {
         bubblesArray[i].update();
@@ -142,7 +180,7 @@ function handleBubbles () {
             bubblesArray.splice(i,1)
         }
         if(bubblesArray[i].distance < bubblesArray[i].radius + player.radius){
-            console.log('vacham');
+            // console.log('vacham');
             if(!bubblesArray[i].counted){
                 score++;
                 bubblesArray[i].counted = true;
@@ -160,8 +198,19 @@ function animate() {
     player.draw();
     ctx.fillStyle = 'blue';
     ctx.fillText('Score : ' + score,10,30)
+    ctx.fillText('50 score and 100 score', 420,30)
     gameFrame++;
+    if (score < 50) {
+        bubbleSound.play();
+    } else if (score < 100){
+        bubbleSound.pause();
+        bubbleSound2.play();
+    } else  {
+        bubbleSound2.pause();
+        bubbleSound3.play()
+    }
+
+
     requestAnimationFrame(animate)
 }
 animate();
-bubbleSound.play();
